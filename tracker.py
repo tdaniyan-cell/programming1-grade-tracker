59835f8e6bb5fd96ee6e1a698e2fa51aff081739
Dannytictic <168723647+Dannytictic@users.noreply.github.com>
2026-08-18 06:28:47 +0400

    Add filter_by_type, filter_by_subject, and filter_by_month

--- tracker.py
+++ tracker.py
@@ -11,11 +11,30 @@ class GradeTracker:
         for a in self.assignments:
             print(a)
 
+    def filter_by_type(self, atype):
+        return [a for a in self.assignments if a.type == atype]
+
+    def filter_by_subject(self, subject):
+        return [a for a in self.assignments if a.subject == subject]
+
+    def filter_by_month(self, month):
+            return [a for a in self.assignments if a.due_date[:7] == month]
+
 if __name__ == "__main__":
     from assignment import Homework, Exam
 
     gt = GradeTracker()
     gt.add_assignment(Homework("Math", "Fractions HW", 85, 100, "2026-10-14"))
-    gt.add_assignment(Exam("Science", "Midterm", 72, 100, "2026-=10-20"))
+    gt.add_assignment(Exam("Science", "Midterm", 72, 100, "2026-10-20"))
+    gt.add_assignment(Homework("Math", "Algebra HW", 90, 100, "2026-11-05"))
+
+    print("--- All ---")
     gt.list_assignments()
-    # print(gt.assignments)
+
+    print("--- Only exams ---")
+    for a in gt.filter_by_type("exam"):
+        print(a)
+
+    print("--- Only October ---")
+    for a in gt.filter_by_month("2026-10"):
+        print(a)
